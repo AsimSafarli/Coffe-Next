e349921e961b25ed88bcd56845013bed75c86061
AsimSafarli <asimseferli014@gmail.com>
2023-06-26 16:31:02 +0400

    Banner uptade

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,10 +1,25 @@
+import Image from 'next/image'
 import React from 'react'
-
+import {AiOutlineShoppingCart} from 'react-icons/ai'
 function Banner() {
   return (
-    <div className='container mx-auto'>
-        <div></div>
-        <div></div>
+    <div className='container mx-auto flex flex-row justify-between gap-x-44'>
+        <div className='flex flex-col gap-y-6'>
+        <div className='text-5xl'>
+        Enjoy your <span className='text-orange'>coffee</span> <br></br>before your activity
+        </div>
+        <div className='text-xl text-gray-500'>Boost your productivity and build your <br/> mood with a glass of coffee in the morning </div>
+        <div className='flex flex-row gap-x-7'>
+          <button className='bg-brown w-max h-max flex flex-row items-center justify-between text-white p-4 rounded-3xl gap-x-3'>Order Now<AiOutlineShoppingCart className=' text-orange  w-7 rounded-full'
+          /></button>
+          <button className='text-orange text-xl'>More Menu</button>
+
+        </div>
+
+        </div>
+        <div>
+          <Image src='/caffe/hero.png' width={500} height={400}/>
+        </div>
     </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 function Navbar() {
   return (
-    <div className='flex flex-row justify-between items-center p-5 container mx-auto'>
-    <div><img src='caffe/Logo.svg'/></div>
-    <div className='flex flex-row items-center gap-x-8'>
-    <div>About us</div>
-    <div>Our Product</div>
-    <div>Delivery</div>
+    <div className="flex flex-row justify-between items-center p-5 container mx-auto">
+      <div>
+        <Image 
+        src="/caffe/Logo.svg"
+        width={60}
+        height={48} />
+      </div>
+      <div className="flex flex-row items-center gap-x-8">
+        <div>About us</div>
+        <div>Our Product</div>
+        <div>Delivery</div>
+      </div>
+      <div className="flex flex-row gap-x-8">
+        <input
+          type="text"
+          placeholder="Cappuccino "
+          className="bg-white shadow-lg placeholder:flex placeholder:items-center placeholder:justify-center placeholder:p-1 rounded-lg h-10 placeholder:mt-3"
+        />
+        <div>
+          <Image src="/caffe/icon.svg" width={32} height={23} />
+        </div>
+      </div>
     </div>
-    <div className='flex flex-row gap-x-8'>
-      <input type='text' placeholder='Cappuccino ' className='bg-white shadow-lg placeholder:flex placeholder:items-center placeholder:justify-center placeholder:p-1 rounded-lg h-10 placeholder:mt-3'/>
-      <div><img src='caffe/icon.svg'/></div>
-    </div>
-    </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
